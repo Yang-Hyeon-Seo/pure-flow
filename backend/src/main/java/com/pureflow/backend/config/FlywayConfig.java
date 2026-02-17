afe8dc4bc1eccaf9bb62bfac1da39fac9ec809d5
DESKTOP-URKE2VL\SSAFY <ihyeonseo@naver.com>
2026-02-17 14:42:10 +0900

    [Fix] Flyway 설정 수정

--- backend/src/main/java/com/pureflow/backend/config/FlywayConfig.java
+++ backend/src/main/java/com/pureflow/backend/config/FlywayConfig.java
@@ -3,11 +3,13 @@ package com.pureflow.backend.config;
 import org.springframework.boot.flyway.autoconfigure.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class FlywayConfig {
 
     @Bean
+    @Profile("local")
     public FlywayMigrationStrategy cleanMigrateStrategy() {
         return flyway -> {
             flyway.clean(); // DB 초기화
